6128081afef87d6243dc169a07a6b237cf0e702b
Fuad Khan <fuad.khan@pbi.com>
2020-11-03 11:56:40 +0200

    new files are created for rest item and authentication study

--- Section_4/code/app.py
+++ Section_4/code/app.py
@@ -1,20 +1,27 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'fuad'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity) #auth
 
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name']== name, items), None) #adding next means the function goes through one item, returns it and goes to the next one
         #adding none means, when the iteration ends, and there is no other items left to iterate, the code returns none
         return {'item': None}, 200 if item else 404 #HTTP error msg
     
     def post(self, name):
-        if next(filter(lambda x: x['name']== name, items), None): #this means if the item exists when a user inputs it
+        if next(filter(lambda x: x['name'] == name, items), None): #this means if the item exists when a user inputs it
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
+        
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
