285e15ac0bc7ec1e5e1b210d411fbbf6b0764625
Fuad Khan <fuad.khan@pbi.com>
2020-11-03 15:33:08 +0200

    complete flask restful web app course section

--- Section_4/code/app.py
+++ Section_4/code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -12,20 +12,38 @@ jwt = JWT(app, authenticate, identity) #auth
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type = float, required = True, help= "this cannot be blank")
+    
     @jwt_required()
     def get(self, name):
-        item = next(filter(lambda x: x['name']== name, items), None) #adding next means the function goes through one item, returns it and goes to the next one
-        #adding none means, when the iteration ends, and there is no other items left to iterate, the code returns none
-        return {'item': None}, 200 if item else 404 #HTTP error msg
+        item = next(filter(lambda x: x['name'] == name, items), None) #adding next means the function goes through one item, returns it and goes to the next one #adding none means, when the iteration ends, and there is no other items left to iterate, the code returns none
+        return {'item': item}, 200 if item else 404 #HTTP error msg
     
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None): #this means if the item exists when a user inputs it
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
         
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201 #201 represent http status code that an item is created
+    
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'item deleted'}
+    
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
 
 class ItemList(Resource):
     def get(self):
